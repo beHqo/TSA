d1533ccf8e88e980a47675f84be491974d305fcf
Behnood Saffari <saffari.behnood@gmail.com>
2022-07-23 11:37:13 +0430

    Update dependency libraries and gradle version

--- app/src/main/java/com/example/android/strikingarts/database/userprefs/UserPreferencesRepository.kt
+++ app/src/main/java/com/example/android/strikingarts/database/userprefs/UserPreferencesRepository.kt
@@ -2,10 +2,9 @@ package com.example.android.strikingarts.database.userprefs
 
 import androidx.datastore.core.DataStore
 import com.example.android.strikingarts.UserPreferences
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class UserPreferencesRepository
 @Inject constructor(userPreferencesStore: DataStore<UserPreferences>) {
-    
+
 }
