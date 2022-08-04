537da4018f5bc2573626f54ee155ead5c78e1a92
Behnood Saffari <saffari.behnood@gmail.com>
2022-08-04 15:15:58 +0430

    Comment out UserPreferences codes because app wouldn't compile with updated gradle dependencies

--- app/src/main/java/com/example/android/strikingarts/database/userprefs/UserPreferencesRepository.kt
+++ app/src/main/java/com/example/android/strikingarts/database/userprefs/UserPreferencesRepository.kt
@@ -1,10 +1,10 @@
-package com.example.android.strikingarts.database.userprefs
-
-import androidx.datastore.core.DataStore
-import com.example.android.strikingarts.UserPreferences
-import javax.inject.Inject
-
-class UserPreferencesRepository
-@Inject constructor(userPreferencesStore: DataStore<UserPreferences>) {
-
-}
+//package com.example.android.strikingarts.database.userprefs
+//
+//import androidx.datastore.core.DataStore
+//import com.example.android.strikingarts.UserPreferences
+//import javax.inject.Inject
+//
+//class UserPreferencesRepository
+//@Inject constructor(userPreferencesStore: DataStore<UserPreferences>) {
+//
+//}
