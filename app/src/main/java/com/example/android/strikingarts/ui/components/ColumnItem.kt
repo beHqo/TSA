e0b02ee29f4781cd0aa081d211edf0d464488728
Behnood Saffari <saffari.behnood@gmail.com>
2022-08-07 19:01:19 +0430

    Added DoubleLineItem template

--- app/src/main/java/com/example/android/strikingarts/ui/components/ColumnItem.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/ColumnItem.kt
@@ -4,8 +4,6 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
-import androidx.compose.material.ListItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,9 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.android.strikingarts.R
 
 @Composable
 fun SingleLineItem(
@@ -30,24 +26,45 @@ fun SingleLineItem(
             .padding(top = 12.dp, bottom = 12.dp, start = 16.dp, end = 16.dp)
             .clickable { onItemClick() }
     ) {
-        Text(
-            text = primaryText,
-            color = MaterialTheme.colors.onSurface,
-            style = MaterialTheme.typography.subtitle1,
-            maxLines = 1,
-            modifier = Modifier
-                .weight(1F)
-        )
-        MoreVertIconButton(
-            modifier = Modifier.size(24.dp), onMoreVertClick
-        )
+        PrimaryText(primaryText, Modifier.weight(1F))
+        MoreVertIconButton(modifier = Modifier.size(24.dp), onMoreVertClick)
     }
 
 }
 
 @Composable
-fun DoubleLineItem() {
-
+fun DoubleLineItem(
+    primaryText: String,
+    secondaryText: String,
+    onItemClick: () -> Unit,
+    onMoreVertClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .heightIn(min = 64.dp)
+            .padding(vertical = 8.dp, horizontal = 16.dp)
+            .clickable { onItemClick() }
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.weight(1F)
+        ) {
+            Text(
+                text = primaryText,
+                color = MaterialTheme.colors.onSurface,
+                style = MaterialTheme.typography.subtitle1,
+                maxLines = 1
+            )
+            Text(
+                text = secondaryText,
+                style = MaterialTheme.typography.caption,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5F)
+            )
+        }
+        MoreVertIconButton(modifier = Modifier.size(24.dp), onMoreVertClick)
+    }
 }
 
 @Composable
@@ -79,25 +96,35 @@ fun DoubleLineItemWithImage(
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.weight(1F)
         ) {
-            Text(
-                text = primaryText,
-                color = MaterialTheme.colors.onSurface,
-                style = MaterialTheme.typography.subtitle1,
-                maxLines = 1
-            )
-            Text(
-                text = secondaryText,
-                style = MaterialTheme.typography.caption,
-                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5F)
-            )
+            PrimaryText(primaryText)
+            SecondaryText(secondaryText)
         }
-        MoreVertIconButton(
-            modifier = Modifier.size(24.dp), onMoreVertClick
-        )
+        MoreVertIconButton(modifier = Modifier.size(24.dp), onMoreVertClick)
     }
 }
 
 @Composable
 fun TripleLineItem() {
 
+}
+
+@Composable
+private fun PrimaryText(primaryText: String, modifier: Modifier = Modifier) {
+    Text(
+        text = primaryText,
+        color = MaterialTheme.colors.onSurface,
+        style = MaterialTheme.typography.subtitle1,
+        maxLines = 1,
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun SecondaryText(secondaryText: String, modifier: Modifier = Modifier) {
+    Text(
+        text = secondaryText,
+        style = MaterialTheme.typography.caption,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5F),
+        modifier = modifier
+    )
 }
