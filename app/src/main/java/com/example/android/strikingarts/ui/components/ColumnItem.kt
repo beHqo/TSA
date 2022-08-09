8cb5359c14d0e892acc36b28a62563b98233dab9
Behnood Saffari <saffari.behnood@gmail.com>
2022-08-09 10:16:24 +0430

    Improve imports

--- app/src/main/java/com/example/android/strikingarts/ui/components/ColumnItem.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/ColumnItem.kt
@@ -1,6 +1,5 @@
 package com.example.android.strikingarts.ui.components
 
-import android.os.TestLooperManager
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -12,7 +11,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
