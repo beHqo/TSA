7f9a39aef36cbc12d063b1fbcc0d9d5f8667a184
Behnood Saffari <saffari.behnood@gmail.com>
2022-08-10 12:33:08 +0430

    ctrl+alt+o

--- app/src/main/java/com/example/android/strikingarts/ui/components/ColumnItem.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/ColumnItem.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
