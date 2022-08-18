c5797bbc3f4fc052bdc967163afbd1d6128b7a71
Behnood Saffari <saffari.behnood@gmail.com>
2022-08-18 18:42:54 +0430

    Remove JosefinaSansFamily and add eczarFamily & InknutAntiquaFamily fonts

--- app/src/main/java/com/example/android/strikingarts/ui/theme/Type.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/theme/Type.kt
@@ -6,125 +6,100 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.example.android.strikingarts.R
 
-private val JosefinSansFamily = FontFamily(
-    Font(R.font.josefinsans_regular),
-    Font(R.font.josefinsans_light, FontWeight.Light),
-    Font(R.font.josefinsans_medium, FontWeight.Medium),
-    Font(R.font.josefinsans_bold, FontWeight.Bold)
+private val eczarFamily = FontFamily(
+    Font(R.font.eczar_regular),
+    Font(R.font.eczar_medium, FontWeight.Medium),
+    Font(R.font.eczar_semibold, FontWeight.SemiBold)
 )
 
-//val UbuntuFamily = FontFamily(
-//    Font(R.font.ubuntu),
-//    Font(R.font.ubuntu_medium, FontWeight.Medium),
-//    Font(R.font.ubuntu_bold, FontWeight.Bold),
-//)
-
+private val inknutAntiquaFamily = FontFamily(
+    Font(R.font.inknut_antiqua_light, FontWeight.Light)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     h1 = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = inknutAntiquaFamily,
         fontWeight = FontWeight.Light,
         fontSize = 96.sp,
         letterSpacing = (-1.5).sp
     ),
     h2 = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = inknutAntiquaFamily,
         fontWeight = FontWeight.Light,
         fontSize = 60.sp,
         letterSpacing = (-0.5).sp
     ),
     h3 = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 48.sp,
         letterSpacing = 0.sp
     ),
     h4 = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 34.sp,
         letterSpacing = (0.25).sp
     ),
     h5 = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
         letterSpacing = 0.sp
     ),
     h6 = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = eczarFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 20.sp,
         letterSpacing = (0.15).em
     ),
     subtitle1 = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         letterSpacing = (0.15).sp
     ),
     subtitle2 = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = eczarFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         letterSpacing = (0.1).sp
     ),
     body1 = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontStyle = FontStyle.Normal,
         fontSize = 16.sp,
         letterSpacing = (0.5).sp
     ),
     body2 = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontStyle = FontStyle.Normal,
         fontSize = 14.sp,
         letterSpacing = (0.25).sp
     ),
     button = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = eczarFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         letterSpacing = (1.25).sp
     ),
     caption = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = eczarFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 12.sp,
         letterSpacing = (0.4).sp
     ),
     overline = TextStyle(
-        fontFamily = JosefinSansFamily,
+        fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 10.sp,
         letterSpacing = (1.5).sp
     )
 )
-
-//    body1 = TextStyle(
-//        fontFamily = FontFamily.Default,
-//        fontWeight = FontWeight.Normal,
-//        fontSize = 16.sp
-//    )
-
-/* Other default text styles to override
-button = TextStyle(
-    fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.W500,
-    fontSize = 14.sp
-),
-caption = TextStyle(
-    fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.Normal,
-    fontSize = 12.sp
-)
-*/
