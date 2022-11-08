a1aaec1ee6e969fcf2ed7fe75925ccf415785fca
Behnood Saffari <saffari.behnood@gmail.com>
2022-11-08 21:53:16 +0330

    Remove ImmutableMap and unused extension function

--- app/src/main/java/com/example/android/strikingarts/utils/ImmutableCollections.kt
+++ app/src/main/java/com/example/android/strikingarts/utils/ImmutableCollections.kt
@@ -4,6 +4,3 @@ import androidx.compose.runtime.Immutable
 
 @Immutable data class ImmutableList<T>(val list: List<T> = emptyList()) : List<T> by list
 @Immutable data class ImmutableSet<T>(val set: Set<T> = emptySet()) : Set<T> by set
-@Immutable data class ImmutableMap<K, V>(val map: Map<K, V> = emptyMap()) : Map<K, V> by map
-
-fun <K, V> Map<K, V>.keysAsImmutableSet() = ImmutableSet(this.keys)
