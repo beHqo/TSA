48ead4fc521a9bc8619e4884803519d4b2f14512
Behnood Saffari <saffari.behnood@gmail.com>
2022-11-13 19:49:28 +0330

    Change the colors of the slider and remove startingNumber and endingNumber parameters

--- app/src/main/java/com/example/android/strikingarts/ui/components/Slider.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/Slider.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Slider
-import androidx.compose.material.SliderDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,30 +14,23 @@ import androidx.compose.ui.unit.dp
 fun DelaySlider(
     value: Float,
     onValueChange: (Float) -> Unit,
+    onValueChangeFinished: () -> Unit,
     valueRange: ClosedFloatingPointRange<Float>,
-    startingNumber: String,
-    finishingNumber: String,
     modifier: Modifier = Modifier
 ) {
 
     Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier) {
-        Text(text = startingNumber, style = MaterialTheme.typography.subtitle2)
+        Text(valueRange.start.toInt().toString(), style = MaterialTheme.typography.subtitle2)
         Slider(
             modifier = Modifier
                 .weight(1F)
                 .padding(start = 4.dp, end = 4.dp),
             value = value,
             onValueChange = onValueChange,
+            onValueChangeFinished = onValueChangeFinished,
             valueRange = valueRange,
             steps = ((valueRange.endInclusive - valueRange.start).minus(1)).toInt(),
-            colors = SliderDefaults.colors(
-                thumbColor = MaterialTheme.colors.secondary,
-                activeTickColor = MaterialTheme.colors.secondary,
-                inactiveTickColor = MaterialTheme.colors.secondary,
-                activeTrackColor = MaterialTheme.colors.secondary,
-                inactiveTrackColor = MaterialTheme.colors.onSurface.copy(alpha = 0.24F)
-            )
         )
-        Text(text = finishingNumber, style = MaterialTheme.typography.subtitle2)
+        Text(valueRange.endInclusive.toInt().toString(), style = MaterialTheme.typography.subtitle2)
     }
 }
