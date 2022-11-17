17c71c450d73052de64e12c205887c10dae943b9
Behnood Saffari <saffari.behnood@gmail.com>
2022-11-17 21:40:30 +0330

    CTRL + L

--- app/src/main/java/com/example/android/strikingarts/ui/theme/Type.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/theme/Type.kt
@@ -16,87 +16,73 @@ private val eczarFamily = FontFamily(
     Font(R.font.eczar_semibold, FontWeight.SemiBold)
 )
 
-private val inknutAntiquaFamily = FontFamily(
-    Font(R.font.inknut_antiqua_light, FontWeight.Light)
-)
+private val inknutAntiquaFamily = FontFamily(Font(R.font.inknut_antiqua_light, FontWeight.Light))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    h1 = TextStyle(
+    defaultFontFamily = eczarFamily, h1 = TextStyle(
         fontFamily = inknutAntiquaFamily,
         fontWeight = FontWeight.Light,
         fontSize = 96.sp,
         letterSpacing = (-1.5).sp
-    ),
-    h2 = TextStyle(
+    ), h2 = TextStyle(
         fontFamily = inknutAntiquaFamily,
         fontWeight = FontWeight.Light,
         fontSize = 60.sp,
         letterSpacing = (-0.5).sp
-    ),
-    h3 = TextStyle(
+    ), h3 = TextStyle(
         fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 48.sp,
         letterSpacing = 0.sp
-    ),
-    h4 = TextStyle(
+    ), h4 = TextStyle(
         fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 34.sp,
         letterSpacing = (0.25).sp
-    ),
-    h5 = TextStyle(
+    ), h5 = TextStyle(
         fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
         letterSpacing = 0.sp
-    ),
-    h6 = TextStyle(
+    ), h6 = TextStyle(
         fontFamily = eczarFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 20.sp,
         letterSpacing = (0.15).em
-    ),
-    subtitle1 = TextStyle(
+    ), subtitle1 = TextStyle(
         fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         letterSpacing = (0.15).sp
-    ),
-    subtitle2 = TextStyle(
+    ), subtitle2 = TextStyle(
         fontFamily = eczarFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         letterSpacing = (0.1).sp
-    ),
-    body1 = TextStyle(
+    ), body1 = TextStyle(
         fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontStyle = FontStyle.Normal,
         fontSize = 16.sp,
         letterSpacing = (0.5).sp
-    ),
-    body2 = TextStyle(
+    ), body2 = TextStyle(
         fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontStyle = FontStyle.Normal,
         fontSize = 14.sp,
         letterSpacing = (0.25).sp
-    ),
-    button = TextStyle(
+    ), button = TextStyle(
         fontFamily = eczarFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         letterSpacing = (1.25).sp
-    ),
-    caption = TextStyle(
+    ), caption = TextStyle(
         fontFamily = eczarFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 12.sp,
         letterSpacing = (0.4).sp
-    ),
-    overline = TextStyle(
+    ), overline = TextStyle(
         fontFamily = eczarFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 10.sp,
