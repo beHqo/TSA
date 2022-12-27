2e183a347afa338562e4443d1b1aeef6c9a33b73
Behnood Saffari <saffari.behnood@gmail.com>
2022-12-27 11:10:15 +0330

    Extract each screen's composable into its own function

--- app/src/main/java/com/example/android/strikingarts/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/navigation/NavGraph.kt
@@ -2,6 +2,7 @@ package com.example.android.strikingarts.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -29,58 +30,89 @@ fun NavGraph(
         navController = navController,
         startDestination = Screen.Technique.route
     ) {
-        composable(
-            route = Screen.Technique.route,
-            arguments = listOf(navArgument(TECHNIQUE_SELECTION_MODE) {
-                type = NavType.BoolType; defaultValue = false
-            })
-        ) {
-            TechniqueListScreen(
-                onNavigateToTechniqueDetails = navController::navigateToTechniqueDetails,
-                onNavigateToComboDetails = navController::navigateFromTechniqueToComboDetails,
-                onSelectionModeChange = onSelectionModeChange,
-            )
-        }
-        composable(
-            route = Screen.TechniqueDetails.route, arguments = listOf(navArgument(TECHNIQUE_ID) {
-                type = NavType.LongType; defaultValue = 0L
-            })
-        ) { TechniqueDetailsScreen(navigateUp = navController::navigateUp) }
-        composable(
-            route = Screen.Combo.route, arguments = listOf(navArgument(COMBO_SELECTION_MODE) {
-                type = NavType.BoolType; defaultValue = false
-            })
-        ) {
-            ComboScreen(
-                navigateToComboDetailsScreen = navController::navigateToComboDetails,
-                onSelectionModeChange = onSelectionModeChange
-            )
-        }
-        composable(
-            route = Screen.ComboDetails.route, arguments = listOf(
-                navArgument(COMBO_ID) { type = NavType.LongType; defaultValue = 0L },
-            )
-        ) {
-            ComboDetailsScreen(
-                onNavigateToTechniqueScreen = {
-                    navController.navigateFromComboDetailsToTechniqueScreen()
-                    onSelectionModeChange(true)
-                },
-                onEnableSelectionMode = onSelectionModeChange,
-                onNavigateUp = navController::navigateToComboScreen
-            )
-        }
-        composable(route = Screen.Workout.route) {
-            WorkoutScreen(
-                navigateToWorkoutDetails = navController::navigateToWorkoutDetails,
-                onSelectionModeChange = onSelectionModeChange
-            )
-        }
-        composable(route = Screen.WorkoutDetails.route) {
-            WorkoutDetailsScreen(
-                navigateUp = navController::navigateUp,
-                navigateToComboScreen = navController::navigateFromWorkoutDetailsToComboScreen
-            )
-        }
+        techniqueScreen(navController, onSelectionModeChange)
+        techniqueDetailsScreen(navController)
+        comboScreen(navController, onSelectionModeChange)
+        comboDetailsScreen(navController, onSelectionModeChange)
+        workoutScreen(navController, onSelectionModeChange)
+        workoutDetailsScreen(navController)
+    }
+}
+
+private fun NavGraphBuilder.techniqueScreen(
+    navController: NavHostController, onSelectionModeChange: (Boolean) -> Unit
+) {
+    composable(
+        route = Screen.Technique.route, arguments = listOf(navArgument(TECHNIQUE_SELECTION_MODE) {
+            type = NavType.BoolType; defaultValue = false
+        })
+    ) {
+        TechniqueListScreen(
+            onNavigateToTechniqueDetails = navController::navigateToTechniqueDetails,
+            onNavigateToComboDetails = navController::navigateFromTechniqueToComboDetails,
+            notifyBottomAppbarOnSelectionMode = onSelectionModeChange,
+        )
+    }
+}
+
+private fun NavGraphBuilder.techniqueDetailsScreen(navController: NavHostController) {
+    composable(
+        route = Screen.TechniqueDetails.route, arguments = listOf(navArgument(TECHNIQUE_ID) {
+            type = NavType.LongType; defaultValue = 0L
+        })
+    ) { TechniqueDetailsScreen(navigateUp = navController::navigateUp) }
+}
+
+private fun NavGraphBuilder.comboScreen(
+    navController: NavHostController, onSelectionModeChange: (Boolean) -> Unit
+) {
+    composable(
+        route = Screen.Combo.route, arguments = listOf(navArgument(COMBO_SELECTION_MODE) {
+            type = NavType.BoolType; defaultValue = false
+        })
+    ) {
+        ComboScreen(
+            navigateToComboDetailsScreen = navController::navigateToComboDetails,
+            onSelectionModeChange = onSelectionModeChange
+        )
+    }
+}
+
+private fun NavGraphBuilder.comboDetailsScreen(
+    navController: NavHostController, onSelectionModeChange: (Boolean) -> Unit
+) {
+    composable(
+        route = Screen.ComboDetails.route, arguments = listOf(
+            navArgument(COMBO_ID) { type = NavType.LongType; defaultValue = 0L },
+        )
+    ) {
+        ComboDetailsScreen(
+            onNavigateToTechniqueScreen = {
+                navController.navigateFromComboDetailsToTechniqueScreen()
+                onSelectionModeChange(true)
+            },
+            onEnableSelectionMode = onSelectionModeChange,
+            onNavigateUp = navController::navigateToComboScreen
+        )
+    }
+}
+
+private fun NavGraphBuilder.workoutScreen(
+    navController: NavHostController, onSelectionModeChange: (Boolean) -> Unit
+) {
+    composable(route = Screen.Workout.route) {
+        WorkoutScreen(
+            navigateToWorkoutDetails = navController::navigateToWorkoutDetails,
+            onSelectionModeChange = onSelectionModeChange
+        )
+    }
+}
+
+private fun NavGraphBuilder.workoutDetailsScreen(navController: NavHostController) {
+    composable(route = Screen.WorkoutDetails.route) {
+        WorkoutDetailsScreen(
+            navigateUp = navController::navigateUp,
+            navigateToComboScreen = navController::navigateFromWorkoutDetailsToComboScreen
+        )
     }
 }
