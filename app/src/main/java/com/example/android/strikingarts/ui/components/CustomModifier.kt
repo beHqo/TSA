7579f0b8e99a992cc30ca26dbfb8cd1faca4026b
Behnood Saffari <saffari.behnood@gmail.com>
2023-01-01 20:03:27 +0330

    Implement clickableWithNoIndication custom modifier

---
+++ app/src/main/java/com/example/android/strikingarts/ui/components/CustomModifier.kt
@@ -0,0 +1,15 @@
+package com.example.android.strikingarts.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+
+fun Modifier.clickableWithNoIndication(onClick: () -> Unit) = composed { then(
+    clickable(
+        onClick = onClick,
+        interactionSource = remember { MutableInteractionSource() },
+        indication = null)
+    )
+}
