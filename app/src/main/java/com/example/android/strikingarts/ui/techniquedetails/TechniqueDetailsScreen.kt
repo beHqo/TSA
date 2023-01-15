1c6ceb40d608603c7324daf75c757e6ba8e4c6fc
Behnood Saffari <saffari.behnood@gmail.com>
2023-01-15 14:26:43 +0330

    Implement DetailsLayout

--- app/src/main/java/com/example/android/strikingarts/ui/techniquedetails/TechniqueDetailsScreen.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/techniquedetails/TechniqueDetailsScreen.kt
@@ -1,107 +1,202 @@
 package com.example.android.strikingarts.ui.techniquedetails
 
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.toggleable
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.text.isDigitsOnly
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.android.strikingarts.BottomSheetBox
+import com.example.android.strikingarts.DetailsLayout
 import com.example.android.strikingarts.R
-import com.example.android.strikingarts.ui.components.ColorPickerDialog
-import com.example.android.strikingarts.ui.components.ColorSample
-import com.example.android.strikingarts.ui.components.ConfirmDialog
-import com.example.android.strikingarts.ui.components.DoubleButtonsRow
+import com.example.android.strikingarts.ui.components.ColorPicker
+import com.example.android.strikingarts.ui.components.DetailsItem
 import com.example.android.strikingarts.ui.components.NameTextField
 import com.example.android.strikingarts.ui.components.NumTextField
 import com.example.android.strikingarts.ui.components.TEXTFIELD_NAME_MAX_CHARS
+import com.example.android.strikingarts.utils.ImmutableSet
 import com.example.android.strikingarts.utils.TechniqueCategory.DEFENSE
 import com.example.android.strikingarts.utils.TechniqueCategory.OFFENSE
+import com.github.skydoves.colorpicker.compose.ColorPickerController
 import com.github.skydoves.colorpicker.compose.rememberColorPickerController
+
+const val TECHNIQUE_NAME_FIELD = 221
+const val TECHNIQUE_NUM_FIELD = 222
+const val TECHNIQUE_MOVEMENT_TYPE = 223
+const val TECHNIQUE_TECHNIQUE_TYPE = 224
+const val TECHNIQUE_COLOR_PICKER = 225
 
 @Composable
 fun TechniqueDetailsScreen(
-    model: TechniqueDetailsViewModel = hiltViewModel(),
-    scrollState: ScrollState = rememberScrollState(),
-    navigateUp: () -> Unit
+    model: TechniqueDetailsViewModel = hiltViewModel(), navigateUp: () -> Unit
 ) {
     val state by model.uiState.collectAsState()
     val colorPickerController = rememberColorPickerController()
+
+    var bottomSheetVisible by rememberSaveable { mutableStateOf(false) }
+    val setBottomSheetVisibility = { value: Boolean -> bottomSheetVisible = value }
+
+    var bottomSheetContent by rememberSaveable { mutableStateOf(0) }
+    val setBottomSheetContent = { value: Int -> bottomSheetContent = value }
+
     val errorState by remember {
         derivedStateOf {
-            state.name.length > TEXTFIELD_NAME_MAX_CHARS ||
-                    !state.num.isDigitsOnly() || state.name.isEmpty() ||
-                    state.num.isEmpty() || state.techniqueType.isEmpty()
+            state.name.length > TEXTFIELD_NAME_MAX_CHARS || !state.num.isDigitsOnly() || state.name.isEmpty() || state.techniqueType.isEmpty()
         }
     }
 
-    if (state.alertDialogVisible) ConfirmDialog(
-        titleId = stringResource(R.string.all_discard),
-        textId = stringResource(R.string.techniquedetails_dialog_discard_changes),
-        confirmButtonText = stringResource(R.string.all_discard),
-        dismissButtonText = stringResource(R.string.all_cancel),
-        onConfirm = navigateUp,
-        onDismiss = model::hideAlertDialog
-    )
+    DetailsLayout(bottomSheetVisible = bottomSheetVisible,
+        onDismissBottomSheet = setBottomSheetVisibility,
+        saveButtonEnabled = !errorState,
+        onSaveButtonClick = { model.onSaveButtonClick(); navigateUp() },
+        onDiscardButtonClick = navigateUp,
+        bottomSheetContent = {
+            when (bottomSheetContent) {
+                TECHNIQUE_NAME_FIELD -> TechniqueNameTextField(
+                    model::onNameChange, setBottomSheetVisibility
+                )
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-            .verticalScroll(scrollState)
-    ) {
+                TECHNIQUE_NUM_FIELD -> TechniqueNumField(
+                    model::onNumChange, setBottomSheetVisibility
+                )
+
+                TECHNIQUE_MOVEMENT_TYPE -> MovementType(
+                    model::onMovementButtonClick, setBottomSheetVisibility
+                )
+
+                TECHNIQUE_TECHNIQUE_TYPE -> TechniqueType(
+                    state.techniqueTypes, model::onTechniqueTypeChange, setBottomSheetVisibility
+                )
+
+                TECHNIQUE_COLOR_PICKER -> TechniqueColorPicker(
+                    colorPickerController, model::onColorChange, setBottomSheetVisibility
+                )
+            }
+
+        }) {
+        TechniqueDetailsColumnContent(
+            name = state.name,
+            num = state.num,
+            movementType = state.movementType,
+            techniqueType = state.techniqueType,
+            color = state.color,
+            setBottomSheetContent = setBottomSheetContent,
+            setBottomSheetVisibility = setBottomSheetVisibility
+        )
+    }
+}
+
+@Composable
+fun TechniqueDetailsColumnContent(
+    name: String,
+    num: String,
+    movementType: String,
+    techniqueType: String,
+    color: String,
+    setBottomSheetContent: (Int) -> Unit,
+    setBottomSheetVisibility: (Boolean) -> Unit
+) {
+    DetailsItem(
+        startText = stringResource(R.string.techniquedetails_technique_category),
+        endText = movementType
+    ) { setBottomSheetContent(TECHNIQUE_MOVEMENT_TYPE); setBottomSheetVisibility(true) }
+    Divider()
+
+    DetailsItem(
+        startText = stringResource(R.string.techniquedetails_textfield_name_helper), endText = name
+    ) { setBottomSheetContent(TECHNIQUE_NAME_FIELD); setBottomSheetVisibility(true) }
+    Divider()
+
+    if (movementType == OFFENSE) DetailsItem(
+        startText = stringResource(R.string.techniquedetails_numfield_helper), endText = num
+    ) { setBottomSheetContent(TECHNIQUE_NUM_FIELD); setBottomSheetVisibility(true) }
+
+    if (movementType == OFFENSE) Divider()
+
+    DetailsItem(
+        startText = stringResource(R.string.techniquedetails_technique_type),
+        endText = techniqueType
+    ) { setBottomSheetContent(TECHNIQUE_TECHNIQUE_TYPE); setBottomSheetVisibility(true) }
+    Divider()
+
+    if (movementType == DEFENSE) DetailsItem(
+        startText = stringResource(R.string.techniquedetails_modify_technique_color),
+        endText = color
+    ) { setBottomSheetContent(TECHNIQUE_COLOR_PICKER); setBottomSheetVisibility(true) }
+}
+
+@Composable
+private fun TechniqueNameTextField(
+    onNameChange: (String) -> Unit, onDismissBottomSheet: (Boolean) -> Unit,
+) {
+    var name by rememberSaveable { mutableStateOf("") }
+    val errorState by remember {
+        derivedStateOf { name.length > TEXTFIELD_NAME_MAX_CHARS || name.isEmpty() }
+    }
+
+    BottomSheetBox(onDismissBottomSheet = onDismissBottomSheet,
+        saveButtonEnabled = !errorState,
+        onSaveButtonClick = { onNameChange(name) }) {
         NameTextField(
-            modifier = Modifier.padding(
-                bottom = if (state.movementType == OFFENSE) 24.dp else 16.dp
-            ),
-            value = state.name,
-            onValueChange = model::onNameChange,
+            modifier = Modifier.padding(bottom = 16.dp),
+            value = name,
+            onValueChange = { if (it.length <= TEXTFIELD_NAME_MAX_CHARS) name = it },
             maxChars = TEXTFIELD_NAME_MAX_CHARS,
             label = stringResource(R.string.techniquedetails_textfield_name_label),
             placeHolder = stringResource(R.string.techniquedetails_textfield_name_hint),
             leadingIcon = R.drawable.ic_glove_filled_light,
-            helperText = stringResource(R.string.technique_details_textfield_name_helper),
+            helperText = stringResource(R.string.techniquedetails_textfield_name_helper),
             imeAction = ImeAction.Next
         )
+    }
+}
 
-        if (state.movementType == OFFENSE) NumTextField(
+@Composable
+private fun TechniqueNumField(
+    onNumChange: (String) -> Unit, onDismissBottomSheet: (Boolean) -> Unit
+) {
+    var num by rememberSaveable { mutableStateOf("") }
+    val errorState by remember { derivedStateOf { !num.isDigitsOnly() || num.isEmpty() } }
+
+    BottomSheetBox(onDismissBottomSheet = onDismissBottomSheet,
+        saveButtonEnabled = !errorState,
+        onSaveButtonClick = { onNumChange(num) }) {
+        NumTextField(
             modifier = Modifier.padding(bottom = 24.dp),
-            value = state.num,
-            onValueChange = model::onNumChange,
+            value = num,
+            onValueChange = { if (it.isDigitsOnly()) num = it },
             label = stringResource(R.string.techniquedetails_numfield_label),
             placeHolder = stringResource(R.string.techniquedetails_numfield_hint),
             leadingIcon = R.drawable.ic_label_filled_light,
-            helperText = stringResource(R.string.technique_details_numfield_helper),
+            helperText = stringResource(R.string.techniquedetails_numfield_helper),
             imeAction = ImeAction.Next,
         )
+    }
+}
 
-        Text(
-            text = stringResource(R.string.techniquedetails_technique_category),
-            style = MaterialTheme.typography.subtitle2,
-            fontSize = 16.sp,
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(bottom = 4.dp)
-        )
+@Composable
+private fun MovementType(
+    onMovementButtonClick: (String) -> Unit, onDismissBottomSheet: (Boolean) -> Unit
+) {
+    var movementType by rememberSaveable { mutableStateOf(DEFENSE) }
 
+    BottomSheetBox(onDismissBottomSheet = onDismissBottomSheet,
+        saveButtonEnabled = true,
+        onSaveButtonClick = { onMovementButtonClick(movementType) }) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -110,57 +205,51 @@ fun TechniqueDetailsScreen(
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
             TechniqueDetailsRadioButton(
-                selected = state.movementType == DEFENSE,
-                onClick = model::onDefenseButtonClick,
+                selected = movementType == DEFENSE,
+                onClick = { movementType = DEFENSE },
                 movementNameId = stringResource(R.string.techniquedetails_defense)
             )
 
             TechniqueDetailsRadioButton(
-                selected = state.movementType == OFFENSE,
-                onClick = model::onOffenseButtonClick,
+                selected = movementType == OFFENSE,
+                onClick = { movementType = OFFENSE },
                 movementNameId = stringResource(R.string.techniquedetails_offense)
             )
         }
-        TechniqueTypeDropdown(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 24.dp),
-            techniqueTypeName = state.techniqueType,
+    }
+}
+
+@Composable
+private fun TechniqueType(
+    techniqueTypeList: ImmutableSet<String>,
+    onTechniqueTypeChange: (String) -> Unit,
+    onDismissBottomSheet: (Boolean) -> Unit
+) {
+    var techniqueType by rememberSaveable { mutableStateOf("") }
+
+    BottomSheetBox(onDismissBottomSheet = onDismissBottomSheet,
+        saveButtonEnabled = true,
+        onSaveButtonClick = { onTechniqueTypeChange(techniqueType) }) {
+        TechniqueTypeDropdown(modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 24.dp),
+            techniqueTypeName = techniqueType,
             textFieldLabel = stringResource(R.string.techniquedetails_technique_type),
-            techniqueTypeList = state.techniqueTypes,
-            onItemClick = model::onTechniqueTypeChange
-        )
+            techniqueTypeList = techniqueTypeList,
+            onItemClick = { techniqueType = it })
+    }
+}
 
-        if (state.movementType == DEFENSE) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .toggleable(value = state.showColorPicker,
-                        onValueChange = { model.showColorPicker() })
-                    .padding(bottom = 24.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = stringResource(R.string.techniquedetails_modify_technique_color),
-                    style = MaterialTheme.typography.subtitle2,
-                    fontSize = 16.sp,
-                    modifier = Modifier.padding(bottom = 16.dp)
-                )
-                if (state.showColorPicker) ColorPickerDialog(
-                    controller = colorPickerController,
-                    techniqueColor = state.color,
-                    onDismiss = model::hideColorPicker,
-                    onColorChange = model::onColorChange
-                ) else ColorSample(controller = null, techniqueColor = state.color)
-            }
-        }
 
-        DoubleButtonsRow(modifier = Modifier.fillMaxWidth(),
-            leftButtonText = stringResource(R.string.all_cancel),
-            rightButtonText = stringResource(R.string.all_save),
-            leftButtonEnabled = true,
-            rightButtonEnabled = !errorState,
-            onLeftButtonClick = model::showAlertDialog,
-            onRightButtonClick = { model.onSaveButtonClick(); navigateUp() })
+@Composable
+private fun TechniqueColorPicker(
+    colorPickerController: ColorPickerController,
+    onColorChange: (String) -> Unit,
+    onDismissBottomSheet: (Boolean) -> Unit,
+) {
+    BottomSheetBox(onDismissBottomSheet = onDismissBottomSheet,
+        saveButtonEnabled = true,
+        onSaveButtonClick = { onColorChange(colorPickerController.selectedColor.value.value.toString()) }) {
+        ColorPicker(colorPickerController)
     }
 }
--- app/src/main/java/com/example/android/strikingarts/ui/techniquedetails/TechniqueDetailsViewModel.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/techniquedetails/TechniqueDetailsViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.android.strikingarts.ui.techniquedetails
 
 import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -13,9 +15,11 @@ import com.example.android.strikingarts.utils.TechniqueCategory.DEFENSE
 import com.example.android.strikingarts.utils.TechniqueCategory.OFFENSE
 import com.example.android.strikingarts.utils.TechniqueCategory.defenseTypes
 import com.example.android.strikingarts.utils.TechniqueCategory.offenseTypes
+import com.example.android.strikingarts.utils.combine6
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -26,9 +30,7 @@ data class TechniqueDetailsUiState(
     val num: String = "",
     val techniqueType: String = "",
     val movementType: String = "",
-    val color: String = "18446744069414584320",
-    val alertDialogVisible: Boolean = false,
-    val showColorPicker: Boolean = false,
+    val color: String = Color.Transparent.value.toString(),
     val techniqueTypes: ImmutableSet<String> = ImmutableSet()
 )
 
@@ -37,111 +39,92 @@ class TechniqueDetailsViewModel @Inject constructor(
     private val repository: TechniqueRepository, private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
     private val techniqueId = savedStateHandle.get<Long>(TECHNIQUE_ID) ?: 0
+
     private val technique = MutableStateFlow(Technique())
-    private val _uiState = MutableStateFlow(TechniqueDetailsUiState())
-    val uiState = _uiState.asStateFlow()
+
+    private val name = MutableStateFlow("")
+    private val num = MutableStateFlow("")
+    private val techniqueType = MutableStateFlow("")
+    private val movementType = MutableStateFlow("")
+    private val color = MutableStateFlow(Color.Transparent.value.toString())
+    private val techniqueTypes = MutableStateFlow(ImmutableSet<String>())
+
+    val uiState = combine6(
+        name, num, techniqueType, movementType, color, techniqueTypes
+    ) { t1, t2, t3, t4, t5, t6 -> TechniqueDetailsUiState(t1, t2, t3, t4, t5, t6) }.stateIn(
+        viewModelScope, SharingStarted.WhileSubscribed(5000L), TechniqueDetailsUiState()
+    )
 
     init {
         initializeTechniqueAndDisplayState()
     }
 
     private fun initializeTechniqueAndDisplayState() {
-        if (techniqueId == 0L) return
-        else viewModelScope.launch {
+        if (techniqueId == 0L) return else viewModelScope.launch {
             repository.getTechnique(techniqueId).also { if (it != null) technique.value = it }
             initialUiUpdate()
         }
     }
 
     private fun initialUiUpdate() {
-        _uiState.update {
-            it.copy(
-                name = savedStateHandle[NAME] ?: technique.value.name,
-                num = savedStateHandle[NUM] ?: technique.value.num,
-                techniqueType = savedStateHandle[TECHNIQUE_TYPE] ?: technique.value.techniqueType,
-                movementType = savedStateHandle[MOVEMENT_TYPE] ?: technique.value.movementType,
-                color = technique.value.color,
-                techniqueTypes = ImmutableSet(
-                    if ((savedStateHandle[TECHNIQUE_TYPE]
-                            ?: technique.value.movementType) == DEFENSE
-                    ) defenseTypes.keys
-                    else offenseTypes.keys
-                )
+        name.update { savedStateHandle[NAME] ?: technique.value.name }
+        num.update { savedStateHandle[NUM] ?: technique.value.num }
+        techniqueType.update { savedStateHandle[TECHNIQUE_TYPE] ?: technique.value.techniqueType }
+        movementType.update { savedStateHandle[MOVEMENT_TYPE] ?: technique.value.movementType }
+        color.update { technique.value.color }
+        techniqueTypes.update {
+            ImmutableSet(
+                if ((savedStateHandle[TECHNIQUE_TYPE]
+                        ?: technique.value.movementType) == DEFENSE
+                ) defenseTypes.keys
+                else offenseTypes.keys
             )
         }
     }
 
     fun onNameChange(value: String) {
         if (value.length <= TEXTFIELD_NAME_MAX_CHARS + 1) {
-            _uiState.update { it.copy(name = value) }
+            name.update { value }
             savedStateHandle[NAME] = value
         }
     }
 
     fun onNumChange(value: String) {
-        _uiState.update { it.copy(num = value) }
-        savedStateHandle[NUM] = value
-    }
-
-    fun onDefenseButtonClick() {
-        _uiState.update {
-            it.copy(
-                movementType = DEFENSE,
-                techniqueType = "",
-                techniqueTypes = ImmutableSet(defenseTypes.keys)
-            )
+        if (value.isDigitsOnly()) {
+            num.update { value }
+            savedStateHandle[NUM] = value
         }
-        savedStateHandle[MOVEMENT_TYPE] = DEFENSE
     }
 
-    fun onOffenseButtonClick() {
-        _uiState.update {
-            it.copy(
-                movementType = OFFENSE, techniqueType = "",
-                color = "18446744069414584320", techniqueTypes = ImmutableSet(offenseTypes.keys)
-            )
-        }
-        savedStateHandle[MOVEMENT_TYPE] = OFFENSE
+    fun onMovementButtonClick(newMovementType: String) {
+        movementType.update { newMovementType }
+        techniqueType.update { "" }
+        techniqueTypes.update { ImmutableSet(if (newMovementType == DEFENSE) defenseTypes.keys else offenseTypes.keys) }
+        color.update { Color.Transparent.value.toString() }
+        savedStateHandle[MOVEMENT_TYPE] = newMovementType
     }
 
-    fun onTechniqueTypeChange(techniqueType: String) {
-        _uiState.update { it.copy(techniqueType = techniqueType) }
-        savedStateHandle[TECHNIQUE_TYPE] = techniqueType
-    }
-
-    fun showColorPicker() {
-        _uiState.update { it.copy(showColorPicker = true) }
-    }
-
-    fun hideColorPicker() {
-        _uiState.update { it.copy(showColorPicker = false) }
+    fun onTechniqueTypeChange(newTechniqueType: String) {
+        techniqueType.update { newTechniqueType }
+        savedStateHandle[TECHNIQUE_TYPE] = newTechniqueType
     }
 
     fun onColorChange(newColor: String) {
-        _uiState.update { it.copy(color = newColor, showColorPicker = false) }
-    }
-
-    fun showAlertDialog() {
-        _uiState.update { it.copy(alertDialogVisible = true) }
-    }
-
-    fun hideAlertDialog() {
-        _uiState.update { it.copy(alertDialogVisible = false) }
+        color.update { newColor }
     }
 
     fun onSaveButtonClick() {
         viewModelScope.launch {
-            if (techniqueId == 0L) repository.insert(technique.value)
-            else repository.update(
+            if (techniqueId == 0L) repository.insert(technique.value) else repository.update(
                 Technique(
                     techniqueId = technique.value.techniqueId,
-                    name = _uiState.value.name,
-                    num = _uiState.value.num,
-                    techniqueType = _uiState.value.techniqueType,
-                    movementType = _uiState.value.movementType,
-                    color = _uiState.value.color,
-                    canBeBodyshot = _uiState.value.movementType == OFFENSE,
-                    canBeFaint = _uiState.value.movementType == OFFENSE
+                    name = name.value,
+                    num = num.value,
+                    techniqueType = techniqueType.value,
+                    movementType = movementType.value,
+                    color = color.value,
+                    canBeBodyshot = movementType.value == OFFENSE,
+                    canBeFaint = movementType.value == OFFENSE
                 )
             )
         }
