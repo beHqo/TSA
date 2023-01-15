115881be3f3d1e6488e3401531314245c8379190
Behnood Saffari <saffari.behnood@gmail.com>
2023-01-15 14:27:20 +0330

    Remove ColorPickerDialog

--- app/src/main/java/com/example/android/strikingarts/ui/components/Dialog.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/Dialog.kt
@@ -5,9 +5,6 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.window.DialogProperties
-import com.github.skydoves.colorpicker.compose.ColorPickerController
-
 
 @Composable
 fun ConfirmDialog(
@@ -25,18 +22,4 @@ fun ConfirmDialog(
         onDismissRequest = onDismiss,
         dismissButton = { TextButton(onDismiss) { Text(dismissButtonText) } },
         confirmButton = { TextButton(onConfirm) { Text(confirmButtonText) } })
-}
-
-@Composable
-fun ColorPickerDialog(
-    controller: ColorPickerController,
-    techniqueColor: String,
-    onColorChange: (String) -> Unit,
-    onDismiss: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    AlertDialog(modifier = modifier,
-        properties = DialogProperties(dismissOnClickOutside = false),
-        onDismissRequest = onDismiss,
-        buttons = { ColorPicker(controller, techniqueColor, onColorChange) })
 }
