f83b2a8a9abcf358e02eb097becae93d74f6b342
Behnood Saffari <saffari.behnood@gmail.com>
2023-01-16 19:12:28 +0330

    Refactor basic components to be moved in the proper package/file

--- app/src/main/java/com/example/android/strikingarts/ui/components/TextField.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/TextField.kt
@@ -1,23 +1,34 @@
 package com.example.android.strikingarts.ui.components
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ContentAlpha
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ExposedDropdownMenuBox
+import androidx.compose.material.ExposedDropdownMenuDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -26,6 +37,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
 import com.example.android.strikingarts.R
+import com.example.android.strikingarts.utils.ImmutableSet
 
 internal const val TEXTFIELD_NAME_MAX_CHARS = 30
 internal const val TEXTFIELD_DESC_MAX_CHARS = 40
@@ -109,4 +121,39 @@ private fun HintText(helperText: String, errorText: String, isError: Boolean) {
         ),
         modifier = Modifier.padding(start = 16.dp)
     )
+}
+
+@OptIn(ExperimentalMaterialApi::class) // ExposedDropdown is experimental
+@Composable
+fun TextFieldItemDropdown(
+    textFieldValue: String,
+    textFieldLabel: String,
+    techniqueTypeList: ImmutableSet<String>,
+    onItemClick: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = { expanded = it }) {
+            TextField(
+                modifier = Modifier.clickable { expanded = !expanded },
+                value = textFieldValue,
+                onValueChange = { },
+                label = { Text(textFieldLabel) },
+                trailingIcon = { DropdownIcon(expanded = expanded) },
+                readOnly = true,
+                colors = ExposedDropdownMenuDefaults.textFieldColors()
+            )
+
+            ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                techniqueTypeList.forEach {
+                    DropdownMenuItem(onClick = {
+                        onItemClick(it)
+                        expanded = false
+                    }) { Text(it) }
+                }
+            }
+        }
+    }
 }
