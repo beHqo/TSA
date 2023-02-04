302384bc55f10dfd22542d7075dda1cf46d30715
Behnood Saffari <saffari.behnood@gmail.com>
2023-02-04 21:46:28 +0330

    Optimize DetailsItemSwitch

--- app/src/main/java/com/example/android/strikingarts/ui/components/Switch.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/Switch.kt
@@ -1,70 +1,94 @@
 package com.example.android.strikingarts.ui.components
 
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.SwipeableState
 import androidx.compose.material.Text
+import androidx.compose.material.rememberSwipeableState
 import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 const val heightSize = 48
+const val tweenDuration = 48
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun DetailsItemSwitch(
+    initialValue: String,
     startingItemText: String,
     endingItemText: String,
-    swipeableState: SwipeableState<String>,
-    onSelectionChange: (String) -> Unit
+    onSelectionChange: (String) -> Unit,
+    modifier: Modifier = Modifier
 ) {
+    val coroutineScope = rememberCoroutineScope()
+
+    val swipeableState = rememberSwipeableState(
+        initialValue = initialValue, animationSpec = tween(tweenDuration)
+    )
+
+    val selectionChange = { newValue: String ->
+        coroutineScope.launch { swipeableState.animateTo(newValue) }
+        onSelectionChange(newValue)
+    }
+
+    LaunchedEffect(key1 = swipeableState.isAnimationRunning) {
+        if (swipeableState.targetValue != initialValue) onSelectionChange(swipeableState.targetValue)
+    }
+
     val halfWidthPx = with(LocalDensity.current) {
         LocalConfiguration.current.screenWidthDp.plus(1).div(2).dp.toPx()
     }
 
-    Box(modifier = Modifier
-        .fillMaxWidth(0.5F)
-        .height(heightSize.dp)
-        .graphicsLayer { translationX = swipeableState.offset.value }
-        .background(color = MaterialTheme.colors.primary))
-
-    Row(
-        modifier = Modifier
+    Box(
+        modifier = modifier
             .fillMaxWidth()
             .height(heightSize.dp)
             .swipeable(
                 state = swipeableState,
                 anchors = mapOf(0F to startingItemText, halfWidthPx to endingItemText),
                 orientation = Orientation.Horizontal
-            ), verticalAlignment = Alignment.CenterVertically
+            )
     ) {
+        Box(modifier = Modifier
+            .fillMaxWidth(0.5F)
+            .height(heightSize.dp)
+            .graphicsLayer { translationX = swipeableState.offset.value }
+            .background(color = MaterialTheme.colors.primary))
         SwitchBox(
-            startingItemText, swipeableState.currentValue == startingItemText
-        ) { onSelectionChange(startingItemText) }
+            startingItemText, Alignment.CenterStart, swipeableState.currentValue == startingItemText
+        ) { selectionChange(startingItemText) }
         SwitchBox(
-            endingItemText, swipeableState.currentValue == endingItemText
-        ) { onSelectionChange(endingItemText) }
+            endingItemText, Alignment.CenterEnd, swipeableState.currentValue == endingItemText
+        ) { selectionChange(endingItemText) }
     }
 }
 
 @Composable
-private fun RowScope.SwitchBox(text: String, selected: Boolean, onClick: () -> Unit) {
+private fun BoxScope.SwitchBox(
+    text: String, alignment: Alignment, selected: Boolean, onClick: () -> Unit
+) {
     Box(
         modifier = Modifier
-            .weight(1F)
             .clickableWithNoIndication(onClick)
+            .fillMaxWidth(0.5F)
+            .fillMaxHeight()
+            .align(alignment)
     ) {
         Text(
             text = text,
