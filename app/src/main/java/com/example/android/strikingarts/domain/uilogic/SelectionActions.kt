6094db1cdaf70144f1df19cc2830c6619d9ea497
Behnood Saffari <saffari.behnood@gmail.com>
2023-02-06 10:54:15 +0330

    selectedItemsIdList now uses ImmutableList

--- app/src/main/java/com/example/android/strikingarts/domain/uilogic/SelectionActions.kt
+++ app/src/main/java/com/example/android/strikingarts/domain/uilogic/SelectionActions.kt
@@ -6,23 +6,25 @@ import kotlinx.coroutines.flow.update
 
 class SelectionActions(
     private val selectionMode: MutableStateFlow<Boolean>,
-    private val selectedItems: MutableStateFlow<List<Long>>
+    private val selectedItemsIdList: MutableStateFlow<ImmutableList<Long>>
 ) {
     fun onItemSelectionChange(id: Long, newSelectedValue: Boolean) {
-        selectedItems.update { if (newSelectedValue) it.plus(id) else it.minus(id) }
+        selectedItemsIdList.update {
+            if (newSelectedValue) ImmutableList(it.plus(id)) else ImmutableList(it.minus(id))
+        }
     }
 
     fun onLongPress(id: Long) {
         if (selectionMode.value) exitSelectionMode()
-        else selectionMode.update { true }; selectedItems.update { listOf(id) }
+        else selectionMode.update { true }; selectedItemsIdList.update { ImmutableList(listOf(id)) }
     }
 
     fun selectAllItems(visibleItemsIds: ImmutableList<Long>) {
-        selectedItems.update { visibleItemsIds }
+        selectedItemsIdList.update { visibleItemsIds }
     }
 
     fun deselectAllItems() {
-        selectedItems.update { emptyList() }
+        selectedItemsIdList.update { ImmutableList() }
     }
 
     fun exitSelectionMode() {
