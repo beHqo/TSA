0e1d3cc008a1528ec51b70ec691bffa7173467eb
Behnood Saffari <saffari.behnood@gmail.com>
2023-02-11 18:11:10 +0330

    Fix a bug which resulted in duplicated techniques not being saved in ComboTechniqueCrossRef

--- app/src/main/java/com/example/android/strikingarts/database/entity/CrossRef.kt
+++ app/src/main/java/com/example/android/strikingarts/database/entity/CrossRef.kt
@@ -1,6 +1,16 @@
 package com.example.android.strikingarts.database.entity
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity(primaryKeys = ["comboId", "techniqueId"])
-data class ComboTechniqueCrossRef(val comboId: Long, val techniqueId: Long)
+//@Entity(primaryKeys = ["refId", "comboId", "techniqueId"])
+@Entity
+data class ComboTechniqueCrossRef(
+    @PrimaryKey(autoGenerate = true) val refId: Long = 0, val comboId: Long, val techniqueId: Long
+)
+//@Entity(primaryKeys = ["comboId, techniqueId"])
+////@Entity(tableName = "combo_technique_ref")
+//data class ComboTechniqueCrossRef(
+////    @PrimaryKey(autoGenerate = true) val refId: Long = 0,
+//    val comboId: Long, val techniqueId: Long
+//)
