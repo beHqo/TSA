6c04075b9b0c13a3939759c0c0729742ede8dc5a
Behnood Saffari <saffari.behnood@gmail.com>
2023-02-14 19:46:27 +0330

    - Initial implementation of outerShadow + Add outerShadow to BottomNavigationBar

--- app/src/main/java/com/example/android/strikingarts/ui/scaffold/BottomNavigationBar.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/scaffold/BottomNavigationBar.kt
@@ -3,10 +3,13 @@ package com.example.android.strikingarts.ui.scaffold
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import com.example.android.strikingarts.ui.components.outerShadow
 import com.example.android.strikingarts.ui.navigation.BottomNavigationItem
 import com.example.android.strikingarts.utils.ImmutableList
 
@@ -14,12 +17,15 @@ import com.example.android.strikingarts.utils.ImmutableList
 fun BottomNavigationBar(
     bottomNavigationItems: ImmutableList<BottomNavigationItem>,
     currentRoute: String,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
-    BottomNavigation(modifier = modifier) {
+    BottomNavigation(
+        modifier = modifier.outerShadow(
+            color = MaterialTheme.colors.primarySurface //TODO: Find better shadow color!
+        )
+    ) {
         bottomNavigationItems.forEach { bottomNavigationItem ->
-            BottomNavigationItem(
-                selected = currentRoute == bottomNavigationItem.route,
+            BottomNavigationItem(selected = currentRoute == bottomNavigationItem.route,
                 onClick = bottomNavigationItem.onClick,
                 label = { Text(bottomNavigationItem.screenName) },
                 icon = { Icon(painterResource(bottomNavigationItem.iconId), null) })
