e8bd114b1e96e31383d40cab38d59848ac3a0809
Behnood Saffari <saffari.behnood@gmail.com>
2023-02-18 11:55:29 +0330

    Implement proper colors for TextButtonOnPrimarySurface

--- app/src/main/java/com/example/android/strikingarts/ui/components/Button.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/Button.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -16,6 +17,7 @@ import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.dp
@@ -71,19 +73,22 @@ fun DoubleTextButtonRow(
 }
 
 @Composable
-fun TextButtonOnPrimary(
+fun TextButtonOnPrimarySurface(
     text: String, onClick: () -> Unit, modifier: Modifier = Modifier, enabled: Boolean
 ) {
     val interactionSource = remember { MutableInteractionSource() }
-    val textColor = contentColorFor(MaterialTheme.colors.primarySurface)
+
+    val backgroundColor = MaterialTheme.colors.primarySurface
+    val contentColor = contentColorFor(backgroundColor)
 
     TextButton(
-        modifier = modifier.indication(
-            interactionSource, rememberRipple(color = MaterialTheme.colors.onPrimary)
+        shape = RectangleShape, colors = ButtonDefaults.buttonColors(
+            backgroundColor = backgroundColor,
+            contentColor = contentColor,
+            disabledBackgroundColor = backgroundColor.copy(ContentAlpha.disabled),
+            disabledContentColor = contentColor.copy(ContentAlpha.disabled),
+        ), modifier = modifier.indication(
+            interactionSource, rememberRipple(color = contentColor)
         ), interactionSource = interactionSource, onClick = onClick, enabled = enabled
-    ) {
-        Text(
-            text, color = textColor.copy(alpha = if (enabled) 1F else ContentAlpha.disabled)
-        )
-    }
+    ) { Text(text.uppercase()) }
 }
