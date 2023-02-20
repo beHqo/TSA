31326cc4dca762a37f7c37893a3f2aa62ed89318
Behnood Saffari <saffari.behnood@gmail.com>
2023-02-20 11:50:26 +0330

    Remove Surface from FilterChip and only use a Box as the parent layout

--- app/src/main/java/com/example/android/strikingarts/ui/components/Chips.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/Chips.kt
@@ -1,47 +1,33 @@
 package com.example.android.strikingarts.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun FilterChip(
-    title: String,
-    selected: Boolean,
-    modifier: Modifier = Modifier,
-    onClick: () -> Unit
+    modifier: Modifier = Modifier, title: String, selected: Boolean, onClick: () -> Unit
+) = Box(
+    modifier = modifier
+        .selectable(selected = selected, onClick = onClick)
+        .shadow(3.dp)
+        .background(if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.surface),
+    contentAlignment = Alignment.Center
 ) {
-    Surface(
-        modifier = modifier
-            .toggleable(
-                value = selected,
-                onValueChange = {
-                    onClick()
-                }
-            ),
-        shape = MaterialTheme.shapes.medium,
-        elevation = 4.dp,
-        color = if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
-    ) {
-        Box(
-            contentAlignment = Alignment.Center,
-        ) {
-            Text(
-                text = if (selected) "✓$title" else title,
-                style = MaterialTheme.typography.body2,
-                color = if (selected)
-                    MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface,
-                modifier =
-                if (selected) Modifier.padding(start = 8.dp, end = 12.dp)
-                else Modifier.padding(horizontal = 8.dp)
-            )
-        }
-    }
+    Text(
+        text = if (selected) "✓$title" else title,
+        style = MaterialTheme.typography.body2,
+        color = contentColorFor(if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.surface),
+        modifier = if (selected) Modifier.padding(start = 8.dp, end = 12.dp)
+        else Modifier.padding(horizontal = 8.dp)
+    )
 }
