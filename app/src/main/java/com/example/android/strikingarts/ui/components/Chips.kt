88a22d5afc34d7c18db392de1cc679d8b8961610
Behnood Saffari <saffari.behnood@gmail.com>
2023-02-26 21:09:37 +0330

    Replace Modifier.background calls with drawBehind to optimize recomposition

--- app/src/main/java/com/example/android/strikingarts/ui/components/Chips.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/Chips.kt
@@ -1,6 +1,5 @@
 package com.example.android.strikingarts.ui.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
@@ -10,24 +9,29 @@ import androidx.compose.material.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun FilterChip(
     modifier: Modifier = Modifier, title: String, selected: Boolean, onClick: () -> Unit
-) = Box(
-    modifier = modifier
-        .selectable(selected = selected, onClick = onClick)
-        .shadow(3.dp)
-        .background(if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.surface),
-    contentAlignment = Alignment.Center
 ) {
-    Text(
-        text = if (selected) "✓$title" else title,
-        style = MaterialTheme.typography.body2,
-        color = contentColorFor(if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.surface),
-        modifier = if (selected) Modifier.padding(start = 8.dp, end = 12.dp)
-        else Modifier.padding(horizontal = 8.dp)
-    )
+    val unSelectedColor = MaterialTheme.colors.surface
+    val selectedColor = MaterialTheme.colors.primary
+
+    Box(
+        modifier = modifier
+            .selectable(selected = selected, onClick = onClick)
+            .shadow(3.dp)
+            .drawBehind { drawRect(color = if (selected) selectedColor else unSelectedColor) },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.body2,
+            color = contentColorFor(if (selected) MaterialTheme.colors.primary else MaterialTheme.colors.surface),
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+    }
 }
--- app/src/main/java/com/example/android/strikingarts/ui/components/SelectableButton.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/SelectableButton.kt
@@ -1,27 +1,46 @@
 package com.example.android.strikingarts.ui.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
+import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
+import com.example.android.strikingarts.ui.shapes.generateHexagonPath
 
 @Composable
-fun RadioButtonWithName(
-    name: String,
-    selected: Boolean,
-    onClick: () -> Unit
+fun SelectableHexagonButton(
+    selected: Boolean, onSelectionChange: (Boolean) -> Unit, modifier: Modifier = Modifier
 ) {
-    Column(Modifier.selectableGroup()) {
-        RadioButton(
-            selected = selected,
-            onClick = onClick
-        )
-        Text(
-            text = name,
-            style = MaterialTheme.typography.body2
-        )
+    val backgroundColor by animateColorAsState(
+        if (selected) MaterialTheme.colors.primarySurface else MaterialTheme.colors.surface,
+        animationSpec = tween(ANIMATION_DURATION)
+    )
+    val borderColor = MaterialTheme.colors.onSurface
+
+    Box(contentAlignment = Alignment.Center, modifier = modifier
+        .size(24.dp)
+        .drawBehind {
+            val path = generateHexagonPath(size.width, size.height)
+
+            drawPath(path = path, style = Stroke(width = 2.dp.toPx()), color = borderColor)
+
+            drawPath(path = path, color = backgroundColor)
+        }
+        .clickableWithNoIndication { onSelectionChange(!selected) }
+        .padding(2.dp)) {
+        FadingAnimatedVisibility(visible = selected) {
+            Text(text = "✓", color = contentColorFor(MaterialTheme.colors.primarySurface))
+        }
     }
 }
