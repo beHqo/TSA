872251fe196d9ec7ad1ca91eafcdbf141085e275
Behnood Saffari <saffari.behnood@gmail.com>
2023-09-09 17:29:55 +0330

    Tidy-up DetailsItem.kt

--- app/src/main/java/com/example/android/strikingarts/ui/components/detailsitem/DetailsItem.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/detailsitem/DetailsItem.kt
@@ -2,7 +2,6 @@ package com.example.android.strikingarts.ui.components.detailsitem
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
@@ -15,9 +14,9 @@ import androidx.compose.ui.unit.dp
 import com.example.android.strikingarts.R
 import com.example.android.strikingarts.ui.components.ColorSample
 import com.example.android.strikingarts.ui.components.PrimaryText
-import com.example.android.strikingarts.ui.components.SelectableHexagonButton
 import com.example.android.strikingarts.ui.theme.designsystemmanager.ColorManager
 import com.example.android.strikingarts.ui.theme.designsystemmanager.ContentAlphaManager
+import com.example.android.strikingarts.ui.theme.designsystemmanager.PaddingManager
 
 @Composable
 fun DetailsItem(
@@ -31,7 +30,7 @@ fun DetailsItem(
         .fillMaxWidth()
         .clickable(onClick = onClick)
         .heightIn(min = 48.dp)
-        .padding(vertical = 8.dp, horizontal = 16.dp)
+        .padding(vertical = PaddingManager.Medium, horizontal = PaddingManager.Large)
 ) {
     PrimaryText(
         text = startText,
@@ -42,26 +41,8 @@ fun DetailsItem(
 }
 
 @Composable
-fun DetailsItem2( //fixme: fix it!
-    modifier: Modifier = Modifier,
-    startText: String,
-    onClick: () -> Unit,
-    endSideSlot: @Composable () -> Unit
-) = Row(
-    verticalAlignment = Alignment.CenterVertically,
-    modifier = modifier
-        .fillMaxWidth()
-        .clickable(onClick = onClick)
-        .heightIn(min = 48.dp)
-        .padding(vertical = 8.dp, horizontal = 16.dp)
-) {
-    PrimaryText(text = startText, textAlpha = ContentAlphaManager.medium)
-    endSideSlot()
-}
-
-@Composable
 fun DetailsItem(
-    modifier: Modifier = Modifier, startText: String, endText: String, onClick: () -> Unit
+    modifier: Modifier = Modifier, startText: String, endText: String, onClick: () -> Unit = {}
 ) = DetailsItem(modifier = modifier, startText = startText, onClick = onClick) {
     PrimaryText(
         text = endText.ifEmpty { stringResource(R.string.all_tap_to_set) },
@@ -77,16 +58,16 @@ fun DetailsItem(
         stringResource(R.string.all_tap_to_set), color = ColorManager.primary
     ) else ColorSample(color)
 }
-
-@Composable
-fun DetailsItem(
-    modifier: Modifier = Modifier,
-    startText: String,
-    selected: Boolean,
-    onSelectionChange: (Boolean) -> Unit
-) = DetailsItem(
-    modifier = modifier,
-    startText = startText,
-    onClick = { onSelectionChange(!selected) }) {
-    SelectableHexagonButton(selected = selected, onSelectionChange = { onSelectionChange(it) })
-}
+//
+//@Composable
+//fun SelectableDetailsItem(
+//    modifier: Modifier = Modifier,
+//    startText: String,
+//    selected: Boolean,
+//    onSelectionChange: (Boolean) -> Unit
+//) = DetailsItem(
+//    modifier = modifier,
+//    startText = startText,
+//    onClick = { onSelectionChange(!selected) }) {
+//    SelectableHexagonButton(selected = selected, onSelectionChange = { onSelectionChange(it) })
+//}
