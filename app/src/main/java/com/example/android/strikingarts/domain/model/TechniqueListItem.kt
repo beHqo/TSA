fe6322652e709e60940eadc855cb3f44c86c1674
Behnood Saffari <saffari.behnood@gmail.com>
2023-09-10 12:19:30 +0330

    Add default value `OFFENSE` for parameter `movementType`

--- app/src/main/java/com/example/android/strikingarts/domain/model/TechniqueListItem.kt
+++ app/src/main/java/com/example/android/strikingarts/domain/model/TechniqueListItem.kt
@@ -2,6 +2,7 @@ package com.example.android.strikingarts.domain.model
 
 import androidx.compose.runtime.Immutable
 import com.example.android.strikingarts.R
+import com.example.android.strikingarts.domain.model.TechniqueCategory.OFFENSE
 
 @Immutable
 data class TechniqueListItem(
@@ -13,6 +14,6 @@ data class TechniqueListItem(
     val audioAttributes: AudioAttributes = SilenceAudioAttributes,
     val color: String = "0",
     val techniqueType: String = "",
-    val movementType: String = "",
+    val movementType: String = OFFENSE,
     val imageRes: Int = R.drawable.none_color
 )
