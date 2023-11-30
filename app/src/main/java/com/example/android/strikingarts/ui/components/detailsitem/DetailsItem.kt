8d7730f3358f9c2a361b6325b0b68b03de5efb95
Behnood Saffari <saffari.behnood@gmail.com>
2023-11-30 19:36:44 +0330

    Initial implementation of a custom layout for displaying DetailsItem with texts vertically if not enough real state is available

--- app/src/main/java/com/example/android/strikingarts/ui/components/detailsitem/DetailsItem.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/components/detailsitem/DetailsItem.kt
@@ -9,7 +9,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.example.android.strikingarts.R
 import com.example.android.strikingarts.ui.components.ColorSample
@@ -18,6 +22,85 @@ import com.example.android.strikingarts.ui.components.SelectableHexagonButton
 import com.example.android.strikingarts.ui.theme.designsystemmanager.ColorManager
 import com.example.android.strikingarts.ui.theme.designsystemmanager.ContentAlphaManager
 import com.example.android.strikingarts.ui.theme.designsystemmanager.PaddingManager
+
+private fun Constraints.hasMinWidth(): Boolean = hasBoundedWidth && minWidth in 1..<maxWidth
+private fun Constraints.hasMinHeight(): Boolean = hasBoundedHeight && minHeight in 1..<maxHeight
+
+@Composable
+private fun DetailsItemLayout(
+    modifier: Modifier = Modifier, startingText: String, endingText: String
+) {
+    val firstComposable = @Composable { StartingText(startingText) }
+    val secondComposable = @Composable { EndingText(endingText) }
+
+    Layout(
+        modifier = modifier,
+        content = { firstComposable(); secondComposable() }) { measurables, constraints ->
+        val placeables =
+            measurables.map { it.measure(constraints.copy(minWidth = 0, minHeight = 0)) }
+
+        val minWidth = placeables.maxOf { it.width }
+        val sumWidth = placeables.sumOf { it.width }
+        val minHeight = placeables.maxOf { it.height }
+        val sumHeight = placeables.sumOf { it.height }
+
+        val cantFitHorizontally = sumWidth > constraints.maxWidth
+
+        val height = when {
+            (constraints.hasBoundedHeight && constraints.hasFixedHeight) -> constraints.maxHeight
+            (constraints.hasBoundedHeight && constraints.hasMinHeight()) -> maxOf(
+                constraints.minHeight, minHeight
+            )
+
+            cantFitHorizontally -> sumHeight
+            else -> minHeight
+        }
+
+        val width = when {
+            (constraints.hasBoundedWidth && constraints.hasFixedWidth) -> constraints.maxWidth
+            (constraints.hasBoundedWidth && constraints.hasMinWidth()) -> maxOf(
+                constraints.minWidth, minWidth
+            )
+
+            cantFitHorizontally -> minWidth
+            else -> sumWidth
+        }
+
+        layout(width, height) {
+            val first = placeables.first()
+            val last = placeables.last()
+
+            val coordinatesSize = coordinates?.size ?: IntSize.Zero
+            val coordinatesHeight = coordinatesSize.height
+            val yPos: Int
+
+            if (cantFitHorizontally) {
+                yPos = (coordinatesHeight - sumHeight) / 2
+
+                first.placeRelative(0, yPos)
+                last.placeRelative(0, yPos + first.height)
+            } else {
+                yPos = (coordinatesHeight - minHeight) / 2
+
+                first.placeRelative(0, yPos)
+                last.placeRelative(coordinatesSize.width - last.width, yPos)
+            }
+        }
+    }
+}
+
+@Composable
+fun DetailsItem(
+    modifier: Modifier = Modifier, startText: String, endText: String, onClick: () -> Unit = {}
+) = DetailsItemLayout(
+    modifier = modifier
+        .clickable(onClick = onClick)
+        .fillMaxWidth()
+        .heightIn(min = 48.dp)
+        .padding(vertical = PaddingManager.Medium, horizontal = PaddingManager.Large),
+    startingText = startText,
+    endingText = endText
+)
 
 @Composable
 fun DetailsItem(
@@ -28,27 +111,18 @@ fun DetailsItem(
 ) = Box(
     contentAlignment = Alignment.CenterEnd,
     modifier = modifier
-        .fillMaxWidth()
         .clickable(onClick = onClick)
+        .fillMaxWidth()
         .heightIn(min = 48.dp)
         .padding(vertical = PaddingManager.Medium, horizontal = PaddingManager.Large)
 ) {
     PrimaryText(
         text = startText,
         textAlpha = ContentAlphaManager.medium,
+        maxLines = 1,
         modifier = Modifier.align(Alignment.CenterStart)
     )
     endSideSlot()
-}
-
-@Composable
-fun DetailsItem(
-    modifier: Modifier = Modifier, startText: String, endText: String, onClick: () -> Unit = {}
-) = DetailsItem(modifier = modifier, startText = startText, onClick = onClick) {
-    PrimaryText(
-        text = endText.ifEmpty { stringResource(R.string.all_tap_to_set) },
-        color = if (endText.isEmpty()) ColorManager.primary else null,
-    )
 }
 
 @Composable
@@ -72,3 +146,16 @@ fun DetailsItem(
     onClick = { onSelectionChange(!selected) }) {
     SelectableHexagonButton(selected = selected, onSelectionChange = { onSelectionChange(it) })
 }
+
+@Composable
+private fun StartingText(text: String) = PrimaryText(
+    text = text, textAlpha = ContentAlphaManager.medium, textAlign = TextAlign.Center, maxLines = 1
+)
+
+@Composable
+private fun EndingText(text: String) = PrimaryText(
+    text = text.ifEmpty { stringResource(R.string.all_tap_to_set) },
+    textAlign = TextAlign.Center,
+    maxLines = 1,
+    color = if (text.isEmpty()) ColorManager.primary else null,
+)
