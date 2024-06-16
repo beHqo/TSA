bc32ae452d291975379a67543ddcd6bb40f4131e
Behnood Saffari <saffari.behnood@gmail.com>
2024-06-16 17:47:09 +0330

    Write tests for `RetrieveTechniqueUseCase`

---
+++ app/src/test/java/com/example/android/strikingarts/domain/usecase/technique/RetrieveTechniqueUseCaseTest.kt
@@ -0,0 +1,26 @@
+package com.example.android.strikingarts.domain.usecase.technique
+
+import com.example.android.strikingarts.data.jab
+import com.example.android.strikingarts.data.repository.FakeTechniqueRepository
+import com.example.android.strikingarts.data.spearElbow
+import com.example.android.strikingarts.domain.model.Technique
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class RetrieveTechniqueUseCaseTest {
+    private val repository = FakeTechniqueRepository()
+    private val useCase = RetrieveTechniqueUseCase(repository)
+
+    @Test
+    fun `Given a database that stores a number of Technique objects, When the supplied id points to an Technique in the database, then it should retrieve that Technique`() =
+        runTest {
+            useCase(jab.id) shouldBe jab
+        }
+
+    @Test
+    fun `Given a database that stores a number of Technique objects, When the supplied id does not point to any Technique in the database, then it should return Technique with default values`() =
+        runTest {
+            useCase(spearElbow.id) shouldBe Technique()
+        }
+}
