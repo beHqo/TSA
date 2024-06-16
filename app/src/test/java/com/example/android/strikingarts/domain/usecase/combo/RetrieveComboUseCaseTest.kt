6a702aae91f01e6a1bac92e6b3e9a08dae4dabe0
Behnood Saffari <saffari.behnood@gmail.com>
2024-06-16 18:39:53 +0330

    Write tests for `RetrieveComboUseCase`

---
+++ app/src/test/java/com/example/android/strikingarts/domain/usecase/combo/RetrieveComboUseCaseTest.kt
@@ -0,0 +1,31 @@
+package com.example.android.strikingarts.domain.usecase.combo
+
+import com.example.android.strikingarts.data.jabCrossJab
+import com.example.android.strikingarts.data.local.assertCombosAreEqual
+import com.example.android.strikingarts.data.repository.FakeComboRepository
+import com.example.android.strikingarts.domain.model.Combo
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class RetrieveComboUseCaseTest {
+    private val repository = FakeComboRepository()
+    private val useCase = RetrieveComboUseCase(repository)
+
+    @Test
+    fun `Given a database pre-populated with Combo objects, When an id of a combo that already exists in the database is supplied, Then it should be retrieved`() =
+        runTest {
+            val combo = jabCrossJab
+
+            val retrieved = useCase(combo.id)
+
+            assertCombosAreEqual(retrieved, combo)
+        }
+
+    @Test
+    fun `Given a database pre-populated with Combo objects, When an id of a combo that does not exists in the database is supplied, Then a Combo with default values should be retrieved`() =
+        runTest {
+            val retrieved = useCase(77)
+
+            assertCombosAreEqual(retrieved, Combo())
+        }
+}
