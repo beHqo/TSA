7d77ff8f98d68d3f370ff97057803d0e35872964
Behnood Saffari <saffari.behnood@gmail.com>
2024-06-17 13:55:58 +0330

    Write tests for 'RetrieveLastExecutedWorkoutResultUseCase'

---
+++ app/src/test/java/com/example/android/strikingarts/domain/usecase/home/RetrieveLastExecutedWorkoutResultUseCaseTest.kt
@@ -0,0 +1,23 @@
+package com.example.android.strikingarts.domain.usecase.home
+
+import com.example.android.strikingarts.data.repository.FakeWorkoutResultRepository
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class RetrieveLastExecutedWorkoutResultUseCaseTest {
+    private val repository = FakeWorkoutResultRepository()
+    private val useCase = RetrieveLastExecutedWorkoutResultUseCase(repository)
+
+    @Test
+    fun `Given a database populated with WorkoutResult objects, When the operator function of SUT is invoked, Then the last successful WorkoutResult should be retrieved`() =
+        runTest {
+            repository.lastSuccessfulWorkoutResult() shouldBe useCase.successful()
+        }
+
+    @Test
+    fun `Given a database populated with WorkoutResult objects, When the operator function of SUT is invoked, Then the last aborted WorkoutResult should be retrieved`() =
+        runTest {
+            repository.lastFailedWorkoutResult() shouldBe useCase.failed()
+        }
+}
