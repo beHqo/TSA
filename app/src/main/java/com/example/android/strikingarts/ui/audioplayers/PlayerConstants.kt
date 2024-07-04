dde6742d5180f9388930250f51876d3bacc0ba1f
Behnood Saffari <saffari.behnood@gmail.com>
2024-07-04 13:06:05 +0330

    Change `Silence.mp3` to `silence.m4a`

--- app/src/main/java/com/example/android/strikingarts/ui/audioplayers/PlayerConstants.kt
+++ app/src/main/java/com/example/android/strikingarts/ui/audioplayers/PlayerConstants.kt
@@ -3,5 +3,5 @@ package com.example.android.strikingarts.ui.audioplayers
 object PlayerConstants {
     internal const val ASSET_TECHNIQUE_PATH_PREFIX = "audio/techniques/"
     internal const val ASSET_SESSION_EVENT_PATH_PREFIX = "audio/sessionevents/"
-    internal const val SILENCE_AUDIO_FILE = "audio/techniques/Silence.mp3"
+    internal const val SILENCE_AUDIO_FILE = "audio/techniques/silence.m4a"
 }
