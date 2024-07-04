b6d22c660420206692a307e21ca544b2c4998400
Behnood Saffari <saffari.behnood@gmail.com>
2024-07-04 14:07:10 +0330

    Manipulate time through `advanceTimeBy` instead of `delay` Directly use the coroutines launched by `testScope` for tests

--- app/src/test/java/com/example/android/strikingarts/domain/usecase/training/ComboVisualPlayerUseCaseTest.kt
+++ app/src/test/java/com/example/android/strikingarts/domain/usecase/training/ComboVisualPlayerUseCaseTest.kt
@@ -4,11 +4,11 @@ import app.cash.turbine.TurbineTestContext
 import app.cash.turbine.test
 import com.example.android.strikingarts.data.longCombo
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestScope
-import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class ComboVisualPlayerUseCaseTest {
@@ -16,6 +16,7 @@ class ComboVisualPlayerUseCaseTest {
     private val testScope = TestScope(testDispatchers)
     private val useCase = ComboVisualPlayerUseCase(testDispatchers)
 
+    @OptIn(ExperimentalCoroutinesApi::class) //advanceTimeBy
     @Test
     fun `SUT should emit colorStrings of the given combo and set isPlaying to true`() = doTest {
         useCase.isPlaying.value shouldBe false
@@ -26,14 +27,13 @@ class ComboVisualPlayerUseCaseTest {
 
         useCase.currentCombo.value shouldBe longCombo
 
-        delay(1)
         longCombo.techniqueList.forEach { technique ->
             val actual = expectMostRecentItem()
             val expected = technique.color
 
             actual shouldBe expected
 
-            delay(technique.audioAttributes.durationMillis)
+            testScope.testScheduler.advanceTimeBy(technique.audioAttributes.durationMillis)
         }
 
         expectNoEvents()
@@ -66,8 +66,7 @@ class ComboVisualPlayerUseCaseTest {
         }
     }
 
-    private fun doTest(testBlock: suspend TurbineTestContext<String>.() -> Unit) =
-        runTest(context = testDispatchers) {
-            testScope.launch { useCase.currentColorString.test(validate = testBlock) }
-        }
+    private fun doTest(testBlock: suspend TurbineTestContext<String>.() -> Unit) {
+        testScope.launch { useCase.currentColorString.test(validate = testBlock) }
+    }
 }
