c575700943edec65e27699e9f6b3b062998f780d
Behnood Saffari <saffari.behnood@gmail.com>
2024-07-07 18:56:26 +0330

    + Remove `PlayableCombo` - Remove `Combo.toPlayableCombo()` mapper

--- app/src/main/java/com/example/android/strikingarts/domain/mapper/ToUiModel.kt
+++ app/src/main/java/com/example/android/strikingarts/domain/mapper/ToUiModel.kt
@@ -4,13 +4,6 @@ import com.example.android.strikingarts.domain.model.Combo
 import com.example.android.strikingarts.domain.model.ImmutableList
 import com.example.android.strikingarts.domain.model.TechniqueRepresentationFormat
 import com.example.android.strikingarts.domain.model.toImmutableList
-import com.example.android.strikingarts.ui.model.PlayableCombo
-
-fun Combo.toPlayableCombo(): PlayableCombo = PlayableCombo(
-    audioStringList = this.getAudioStringList(),
-    audioDurationMillis = this.getAudioDuration(),
-    delayMillis = this.delayMillis,
-)
 
 fun Combo.getAudioStringList(): ImmutableList<String> =
     this.techniqueList.map { it.audioAttributes.audioString }.toImmutableList()
--- app/src/main/java/com/example/android/strikingarts/ui/model/PlayableCombo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.android.strikingarts.ui.model
-
-import com.example.android.strikingarts.domain.model.ImmutableList
-
-data class PlayableCombo(
-    val audioStringList: ImmutableList<String> = ImmutableList(),
-    val audioDurationMillis: Long = 0,
-    val delayMillis: Long = 0,
-)
